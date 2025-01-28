04f15c7dd619895436d8e91bb20daef5e316eeeb
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-01-28 13:15:38 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Имя: {FirstName}");
+        Console.WriteLine($"Фамилия: {LastName}");
+        Console.WriteLine($"Возраст: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person
+        {
+            FirstName = "Олег",
+            LastName = "Тиньков",
+            Age = 57
+        };
+
+        person.DisplayInfo();
+        Console.ReadKey();
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Address
+{
+    public string Index { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string House { get; set; }
+    public string Apartment { get; set; }
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine($"Индекс: {Index}");
+        Console.WriteLine($"Страна: {Country}");
+        Console.WriteLine($"Город: {City}");
+        Console.WriteLine($"Улица: {Street}");
+        Console.WriteLine($"Дом: {House}");
+        Console.WriteLine($"Квартира: {Apartment}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Address address = new Address
+        {
+            Index = "98001",
+            Country = "США",
+            City = "Вашингтон",
+            Street = "Пенсильвания-авеню",
+            House = "110",
+            Apartment = "125"
+        };
+
+        address.DisplayAddress();
+        Console.ReadKey();
+    }
+}
